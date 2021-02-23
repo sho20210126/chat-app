f0e499ec830a452dc89ecf7ba0f415fcd4170bf6
sho20210126 <shohei210321@gmail.com>
2021-02-23 21:26:38 +0900

    結合テストの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
   def index
-    binding.pry
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
